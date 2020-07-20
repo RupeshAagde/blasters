4a830f821ad7257e3607bd431214edcf19c28447
Abhay Gawade <abhaygawade@gofynd.com>
2020-07-20 10:34:27 +0000

    - single loader - inline svg generator fixes - blaster-scrollbar name fix - added grindor and image uploader for images - adm-no-content to page-empty transition - new way to run project locally `npm run local`

--- src/pages/settings/basic-details.vue
+++ src/pages/settings/basic-details.vue
@@ -15,7 +15,11 @@
         </page-header>
         <div class="main-container">
             <loader v-if="pageLoading"></loader>
-            <section>
+            <page-error
+                v-else-if="pageError && !pageLoading"
+                @tryAgain="fetchSettings"
+            ></page-error>
+            <section v-show="!pageLoading && !pageError">
                 <label class="cl-Mako bold-sm title">Basic Details</label>
                 <div class="input-row">
                     <!-- Title -->
