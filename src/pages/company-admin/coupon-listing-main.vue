e5cfa221858dec3d39ea1cd58b4418642eeb7fa2
omanshlavania <omanshlavania@gofynd.com>
2021-11-16 21:30:00 +0530

    modal-ui

--- src/pages/company-admin/coupon-listing-main.vue
+++ src/pages/company-admin/coupon-listing-main.vue
@@ -2,12 +2,86 @@
     <div class="panel">
         <div class="main-container">
             <div style="width: 100%">
-                <jumbotron
-                    :title="'Coupons'"
-                    :desc="'View All Coupons'"
-                ></jumbotron>
-                <coupon-listing style="margin-top:24px"></coupon-listing> 
+                <jumbotron :title="'Coupons'" :desc="'View All Coupons'">
+                    <nitrozen-button
+                        id="create"
+                        :theme="'secondary'"
+                        @click="openModal"
+                        v-flatBtn
+                        >Create</nitrozen-button
+                    >
+                </jumbotron>
+                <coupon-listing style="margin-top: 24px"></coupon-listing>
             </div>
+            <nitrozen-dialog
+                class="remove_staff_dialog desc-dialog"
+                ref="coupon_create_dialog"
+                title="Create Coupon"
+                @close="closeModal"
+            >
+                <!-- <template slot="header">
+                    <div class="capitalize">Create</div>
+                </template> -->
+
+                <template slot="body" class="desc-dialog">
+                    <div class="parent">
+                        <div class="left">
+                            <div class="sidebar">
+                                 
+                        <div class="childNameContainer"
+                         v-bind:class="{
+                            childSelected: selectedType == x.key
+                        }"
+                         v-for="x in titleList" :key="x.key">
+                        <div class="childName" @click="()=>{ selectedType = x.key   }">{{x.title}}</div>
+                        
+                    </div>
+                            </div>
+                        </div>
+                        <div class="right">
+                            <div>
+                                <div
+                                    class="page-content global-font-family"
+                                    
+                                >
+                                    <div class="title bold-sm cl-Mako">
+                                        {{ title }}
+                                    </div>
+                                    <div
+                                        class="
+                                            subtitle
+                                            regular-xs
+                                            cl-DustyGray2
+                                        "
+                                    >
+                                        {{ subtitle }}
+                                    </div>
+                                    <div class="title dark-xs cl-Mako">
+                                        Example:
+                                    </div>
+                                    <pre
+                                        class="sample-text"
+                                        v-html="description"
+                                    ></pre>
+                                </div>
+                            </div>
+                            <div></div>
+                        </div>
+                    </div>
+                </template>
+                <template slot="footer">
+                    <div>
+                        <nitrozen-button
+                            v-flatBtn
+                            class="mr24"
+                            id="approve"
+                            :theme="'secondary'"
+                            :disabled="false"
+                            >Select and Proceed</nitrozen-button
+                        >
+                    </div>
+                </template>
+            </nitrozen-dialog>
         </div>
     </div>
 </template>
@@ -26,30 +100,166 @@
         margin-top: 110px !important;
     }
 }
+.left {
+    flex-basis: 300px;
+    border-right: 1px solid #d0d0d0;
+    overflow: hidden;
+    padding-top: 12px;
+    .sidebar {
+        
+        .group {
+            margin-bottom: 12px;
+        }
+        .groupNameContainer {
+            padding: 6px 0px;
+            width: 100%;
+            color: #9b9b9b;
+            font-family: Inter;
+            font-size: 12px;
+            letter-spacing: 0;
+            line-height: 17px;
+            .groupName {
+                margin: 0px 12px;
+            }
+        }
+        .childNameContainer {
+            display: flex;
+            padding: 6px 6px;
+            color: #212121;
+            font-family: Inter;
+            font-size: 14px;
+            letter-spacing: 0;
+            line-height: 23px;
+            cursor: pointer;
+            .childName {
+                margin: 0px 12px;
+                flex: 4;
+            }
+            .child-linked {
+                flex: 1;
+            }
+        }
+        .childNameContainer:hover {
+            background-color: rgba(92, 107, 221, 0.12);
+        }
+        .childSelected {
+            background-color: rgba(92, 107, 221, 0.12);
+        }
+        .page-error {
+            width: auto;
+            img {
+                width: 100%;
+                height: auto;
+            }
+        }
+    }
+}
+.right {
+    flex-basis: 100%;
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    overflow: auto;
+    height: auto;
+    max-height: 100%;
+    padding: 24px;
+    flex-basis: 100%;
+    border-bottom: 1px solid #d0d0d0;
+}
+.parent {
+    display: flex;
+}
+.page-content {
+    font-family: Inter, sans-serif;
+}
+.title {
+    margin-bottom: 12px;
+}
+.subtitle {
+    margin-bottom: 20px;
+}
+.sample-text {
+    font-family: inherit;
+    .font-size(@font-xs);
+    background: #f2f2f2;
+    line-height: 20px;
+    ::v-deep b {
+        font-weight: 600;
+    }
+}
+.desc-dialog{
+    ::v-deep.nitrozen-dialog-body{
+     padding: 0px;
+    }
+    ::v-deep.nitrozen-dialog-header{
+        margin-bottom: 0px;
+    }
+}
 
+    
 </style>
 
 <script>
 import Jumbotron from '@/components/common/jumbotron';
-import CouponsListing from "./coupons-listing";
+import CouponsListing from './coupons-listing';
+import {
+    NitrozenButton,
+    flatBtn,
+    strokeBtn,
+    NitrozenInput,
+    NitrozenDropdown,
+    NitrozenDialog,
+    NitrozenRadio,
+    NitrozenPagination,
+} from '@gofynd/nitrozen-vue';
+import { TYPE_DATA } from '@/helper/coupon-helper';
 
 export default {
-    name:'coupon-listing-main',
-    components:{
+    name: 'coupon-listing-main',
+    components: {
+        'nitrozen-dropdown': NitrozenDropdown,
+        'nitrozen-button': NitrozenButton,
+        'nitrozen-input': NitrozenInput,
+        'nitrozen-pagination': NitrozenPagination,
+        'nitrozen-dialog': NitrozenDialog,
+        'nitrozen-radio': NitrozenRadio,
         jumbotron: Jumbotron,
-        'coupon-listing':CouponsListing,
+        'coupon-listing': CouponsListing,
     },
-    computed:{
-
+    directives: {
+        flatBtn,
+        strokeBtn,
     },
-    data(){
-        return {}
+    computed: {
+        title() {
+            return TYPE_DATA[this.selectedType].title;
+        },
+        subtitle() {
+            return TYPE_DATA[this.selectedType].subtitle;
+        },
+        description() {
+            return TYPE_DATA[this.selectedType].description;
+        },
     },
-    mounted(){
-
+    data() {
+        return {
+            selectedType: 0,
+            titleList: [...TYPE_DATA],
+        };
     },
-    methods:{
-
-    }
-}
+    mounted() {
+    },
+    methods: {
+        openModal: function () {
+           
+            this.$refs['coupon_create_dialog'].open({
+                width: '1100px',
+                height: '600px',
+            });
+        },
+        closeModal() {
+            this.$refs['coupon_create_dialog'].close();
+        },
+    },
+};
 </script>
--- src/pages/company-admin/coupons-listing.vue
+++ src/pages/company-admin/coupons-listing.vue
@@ -43,12 +43,54 @@
         </div>
         <adm-shimmer v-if="loading" :count="4"></adm-shimmer>
         <div v-else-if="initial_data.length" class="coupon-card-list">
-            <!-- <list-card
-                v-for="coupon in initial_data"
-                :key="coupon._id"
-                :coupon="coupon"
-                @click="edit_coupon(coupon._id, $event)"
-            ></list-card> -->
+            <div
+        class="blaster-list-card-container coupon-card-container"
+        
+    >
+        <!-- <div class="card-avatar banner-image">
+            <img :src="'/public/admin/assets/pngs/default_icon_listing.png'" />
+        </div> -->
+        <div class="card-content-section left-container">
+            <div class="card-content-line-1">
+                {{ coupon.title }}
+            </div>
+            <div class="darker-xs card-content-line-2 coupon-code">
+                {{ coupon.code.toUpperCase() }}
+                <span @click="onCopyCode">
+                    <inline-svg
+                        title="Click to copy"
+                        :src="'copy'"
+                        class="copy-code"
+                    ></inline-svg>
+                </span>
+            </div>
+            <div class="card-content-line-3">
+                {{
+                    `Created on ${new Date(
+                        coupon.created_on
+                    ).toDateString()}`
+                }}
+                <!-- <span v-if="showNextSchedule()">
+                    |
+                    <span class="show-next-schedule">
+                        {{ showNextSchedule() }}
+                    </span>
+                </span> -->
+            </div>
+        </div>
+        <div class="card-badge-section right-container">
+            <div class="states">
+                <nitrozen-badge v-if="coupon.live" state="success"
+                    >Live</nitrozen-badge
+                >
+                <nitrozen-badge v-else>Not Live</nitrozen-badge>
+                <nitrozen-badge v-if="true" state="success"
+                    >Active</nitrozen-badge
+                >
+                <nitrozen-badge v-else>Inactive</nitrozen-badge>
+            </div>
+        </div>
+    </div>
         </div>
         <div v-else>
             <no-content :helperText="'No coupons found'"></no-content>
@@ -58,16 +100,19 @@
             name="coupons"
             v-model="filter_data.pagination"
             :pageSizeOptions="perPageValues"
-            @change="pageOptionChange"
+            
         ></nitrozen-pagination>
     </div>
 
 </template>
 <script>
 import {
-    debounce,
+    debounce, copyToClipboard
 } from '@/helper/utils';
+
 import PageEmpty from '@/components/common/page-empty.vue';
+import inlinesvg from '@/components/common/ukt-inline-svg.vue';
+
 import {
     NitrozenButton,
     flatBtn,
@@ -77,6 +122,7 @@ import {
     NitrozenDialog,
     NitrozenRadio,
     NitrozenPagination,
+    NitrozenBadge
 } from '@gofynd/nitrozen-vue';
 import {TYPE_DATA} from '@/helper/coupon-helper'
 export default {
@@ -88,12 +134,14 @@ export default {
         'nitrozen-pagination': NitrozenPagination,
         'nitrozen-dialog': NitrozenDialog,
         'nitrozen-radio': NitrozenRadio,
+        'nitrozen-badge': NitrozenBadge,
+        'inline-svg' : inlinesvg,
     'no-content': PageEmpty,
     },
     data() {
         return {
             query_string: '',
-            initial_data: [],
+            initial_data: [1],
             loading: false,
             show_schedule_modal: false,
             showSelectModal: false,
@@ -113,7 +161,14 @@ export default {
                 }
             },
             perPageValues: [1, 2, 5, 10, 25, 50, 100, 200],
-            searchText: ''
+            searchText: '',
+            coupon: {
+                title: 'Testing UI',
+                code: 'test22',
+                created_on : "2019-11-06T16:16:53.982Z",
+                live: true
+            }
+
         }
     },
     methods: {
@@ -135,8 +190,14 @@ export default {
             });
         },
         get_coupons_list(){
-        console.log('hi');
-        }
+        },
+        onCopyCode(event) {
+            copyToClipboard(this.coupon.code.toUpperCase());
+            this.$snackbar.global.showInfo('Copied to clipboard', 1000);
+            event.stopPropagation();
+            event.preventDefault();
+        },
+       
         
     },
     computed: {
@@ -204,9 +265,58 @@ export default {
         }
     }
 
+
     .nitrozen-pagination-container,
     .coupon-card-list {
         margin-top: 24px;
     }
+    .coupon-card-container {
+    .banner-image {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        overflow: hidden;
+        @media @mobile {
+            width: 15%;
+            height: 75%;
+        }
+        img {
+            width: auto;
+            height: 100%;
+        }
+    }
+    .left-container {
+        .coupon-code {
+            display: flex;
+            position: relative;
+            align-items: center;
+            .copy-code {
+                width: 14px;
+                padding-left: 5px;
+                /deep/ svg {
+                    pointer-events: none;
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+        }
+        .show-next-schedule {
+            color: #00c853;
+        }
+    }
+    .right-container {
+        flex: 0.5;
+        position: relative;
+        @media @mobile {
+            flex: none;
+        }
+        ::v-deep .nitrozen-badge {
+            pointer-events: none;
+        }
+    }
+}
 }
 </style>
