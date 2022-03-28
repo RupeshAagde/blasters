3d6957d1754cd7b0f5897e2d90e28b3377c761c1
Rushabh Mulraj Shah <rushabhmshah@gofynd.com>
2022-03-28 17:29:21 +0530

    ID:FPCO-2047;DONE:100;HOURS:1; Resolving the bug

--- src/pages/super-user/user-access.vue
+++ src/pages/super-user/user-access.vue
@@ -473,6 +473,7 @@ export default {
                     .finally(() => {
                         if (this.userId !== searchUserId) {
                             this.userId = searchUserId;
+                            this.pagination.current = 1;
                             this.fetchUsers();
                         }
                     });
