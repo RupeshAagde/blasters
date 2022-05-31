3ce031dd6613ee6a375160fa869f01414826a24e
Rohan Shah <rohanshah@gofynd.com>
2022-05-31 14:00:44 +0530

    Packaging home init

--- src/pages/packaging/packaging-home.vue
+++ src/pages/packaging/packaging-home.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>Packaging home</div>
+  <div>
+    <page-header :title="'Packaging'"/>
+    <div class="packaging-home-container">
+      <!-- Call the add package and list package components here -->
+      Packaging home
+    </div>
+    </div>
 </template>
 
 <script>
+import PageHeader from "../../components/common/layout/page-header.vue"
 export default {
 name: 'packaging-home',
+components:{
+ PageHeader
+}
 }
 </script>
-
-<style>
-
+<style lang="less">
+@import '../../less/packaging/packaging-home.less';
 </style>
