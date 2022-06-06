8adb0e87476f4d112574da4f733f083eeec9c1c9
aayush jain <aayushvjain@gofynd.com>
2022-06-06 12:18:55 +0530

    ID:FPCO-2637; DONE:80; HOURS:32; Changing the object for items

--- src/pages/settings/extensions-listing/index.vue
+++ src/pages/settings/extensions-listing/index.vue
@@ -13,45 +13,54 @@
 import Vue from 'vue';
 import NoSSR from 'vue-no-ssr';
 import Container from './container.vue';
+import root from 'window-or-global';
 
 export default {
-    name: 'extensions-listing', 
+    name: 'extensions-listing',
     components: {
         'no-ssr': NoSSR,
-        'container': Container
+        container: Container
     },
     mounted() {
-        import(
-            /*webpackChunkName:"nitrozen-snackbar" */ '@gofynd/nitrozen-vue/src/components/NSnackbar'
-        ).then((NitrozenSnackbar) => {
-            Vue.use(NitrozenSnackbar.default);
-            Vue.snackbar.register('show', (message) => message, {
-                position: 'top-center',
-                duration: 2000
+        /* istanbul ignore if */
+        if (
+            !root.process ||
+            (root.process &&
+                root.process.env &&
+                root.process.env.NODE_ENV != 'test')
+        ) {
+            import(
+                /*webpackChunkName:"nitrozen-snackbar" */ '@gofynd/nitrozen-vue/src/components/NSnackbar'
+            ).then((NitrozenSnackbar) => {
+                Vue.use(NitrozenSnackbar.default);
+                Vue.snackbar.register('show', (message) => message, {
+                    position: 'top-center',
+                    duration: 2000
+                });
+                Vue.snackbar.register('showSuccess', (message) => message, {
+                    position: 'top-center',
+                    duration: 2000,
+                    type: 'success'
+                });
+                Vue.snackbar.register('showError', (message) => message, {
+                    position: 'top-center',
+                    duration: 2000,
+                    type: 'error'
+                });
+                Vue.snackbar.register('showWarning', (message) => message, {
+                    position: 'top-center',
+                    duration: 2000,
+                    type: 'warning'
+                });
+                Vue.snackbar.register('showInfo', (message) => message, {
+                    position: 'top-center',
+                    duration: 2000,
+                    type: 'infor'
+                });
             });
-            Vue.snackbar.register('showSuccess', (message) => message, {
-                position: 'top-center',
-                duration: 2000,
-                type: 'success'
-            });
-            Vue.snackbar.register('showError', (message) => message, {
-                position: 'top-center',
-                duration: 2000,
-                type: 'error'
-            });
-            Vue.snackbar.register('showWarning', (message) => message, {
-                position: 'top-center',
-                duration: 2000,
-                type: 'warning'
-            });
-            Vue.snackbar.register('showInfo', (message) => message, {
-                position: 'top-center',
-                duration: 2000,
-                type: 'infor'
-            });
-        });
+        }
     }
-}
+};
 </script>
 
 <style lang="less" scoped>
@@ -59,7 +68,7 @@ export default {
     height: 100%;
     .section-content {
         // height: 100%;
-        
+
         > div {
             height: 100%;
             display: flex;
@@ -83,7 +92,7 @@ export default {
     }
 
     ::placeholder {
-        font-family: Inter !important;    
+        font-family: Inter !important;
     }
 }
 </style>
