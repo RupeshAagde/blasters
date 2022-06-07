ce2a6f687f3fe5c26ccc3112c7d13bb91d45d4fc
Rohan Shah <rohanshah@gofynd.com>
2022-06-07 15:36:03 +0530

    bulk-packaging card setup

---
+++ src/components/packaging/common/bulk-packaging-card.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="bulk-card-container">
+        <div class="bulk-card-header">Category config and toggle</div>
+        <div class="bulk-card-body">
+            <div class="bulk-card-textfield-group">
+                <p>Volumetric Weight</p>
+                <div class="row-group">
+                    <div
+                        v-for="(input, index) of Object.keys(
+                            inputs.volumetricWeight
+                        )"
+                        :key="'volumetric-field' + index"
+                        :id="'volumetric-field' + index"
+                        class="row-group-input"
+                    >
+                        <nitrozen-input
+                            class="input w-l"
+                            :label="inputs.volumetricWeight[input].label"
+                            :required="true"
+                            :placeholder="
+                                inputs.volumetricWeight[input].placeholder
+                            "
+                            v-model="inputs.volumetricWeight[input].value"
+                            type="number"
+                            @blur="handleBlur('volumetricWeight', input)"
+                            @input="
+                                (val) =>
+                                    handleChange('volumetricWeight', input, val)
+                            "
+                        />
+                        <nitrozen-error
+                            v-if="inputs.volumetricWeight[input].error"
+                        >
+                            {{ inputs.volumetricWeight[input].error }}
+                        </nitrozen-error>
+                    </div>
+                </div>
+            </div>
+            <div class="bulk-card-textfield-group">
+                <p>Quantity</p>
+                <div class="row-group">
+                    <div
+                        v-for="(input, index) of Object.keys(inputs.quantity)"
+                        :key="'quantity-field' + index"
+                        :id="'quantity-field' + index"
+                        class="row-group-input"
+                    >
+                        <nitrozen-input
+                            class="input w-l"
+                            :label="inputs.quantity[input].label"
+                            :required="true"
+                            :placeholder="inputs.quantity[input].placeholder"
+                            v-model="inputs.quantity[input].value"
+                            type="number"
+                            @blur="handleBlur('quantity', input)"
+                            @input="
+                                (val) => handleChange('quantity', input, val)
+                            "
+                        />
+                        <nitrozen-error
+                            v-if="inputs.quantity[input].error"
+                        >
+                            {{ inputs.quantity[input].error }}
+                        </nitrozen-error>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {
+    NitrozenInput,
+    NitrozenError,
+    NitrozenToggleBtn,
+    NitrozenDropdown
+} from '@gofynd/nitrozen-vue';
+export default {
+    name: 'bulk-packaging-card',
+    components: {
+        NitrozenInput,
+        NitrozenError,
+        NitrozenToggleBtn,
+        NitrozenDropdown
+    },
+    props: {
+        inputs: {
+            type: Object
+        }
+    }
+};
+</script>
+
+<style></style>
