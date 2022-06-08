e2156ad5de826e7afa172755486beaa6d12a68a3
Rohan Shah <rohanshah@gofynd.com>
2022-06-08 17:53:14 +0530

    mutation added for setting category data

--- src/store/modules/packaging.module.js
+++ src/store/modules/packaging.module.js
@@ -1,47 +1,40 @@
-import {
-    FETCH_CATEGORIES,
-    FETCH_PACKAGING_PRODUCTS
-} from '../action.type';
+import { FETCH_CATEGORIES, FETCH_PACKAGING_PRODUCTS } from '../action.type';
 
-import {
-    GET_CATEGORIES,
-    GET_PACKAGING_PRODUCTS
-} from '../getters.type';
+import { GET_CATEGORIES, GET_PACKAGING_PRODUCTS } from '../getters.type';
 
-import { 
-   SET_PACKAGING_PRODUCTS
-} from '../mutation.type';
-
-
+import { SET_CATEGORIES, SET_PACKAGING_PRODUCTS } from '../mutation.type';
 
 const getDefaultState = () => {
     return {
-        products:[],
-        categories:[]
+        products: [],
+        categories: []
     };
 };
 
 const getters = {
     [GET_PACKAGING_PRODUCTS](state) {
-        return state.products
+        return state.products;
     },
     [GET_CATEGORIES](state) {
-        return state.categories
-    },
+        return state.categories;
+    }
 };
 
 const mutations = {
     [SET_PACKAGING_PRODUCTS](state, data) {
-       state.products = data;
+        state.products = data;
     },
+    [SET_CATEGORIES](state, data) {
+        state.categories = data;
+    }
 };
 
 const actions = {
     [FETCH_PACKAGING_PRODUCTS]({ commit }, params) {
-        return []
+        return [];
     },
     [FETCH_CATEGORIES]({ commit }, params) {
-        return []
+        return [];
     }
 };
 
