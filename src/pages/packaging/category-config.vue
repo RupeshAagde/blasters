63795d30b9e307d1d3ffb30197d03f462898fcb5
Rohan Shah <rohanshah@gofynd.com>
2022-06-08 16:49:17 +0530

    Go back added for home pages

--- src/pages/packaging/category-config.vue
+++ src/pages/packaging/category-config.vue
@@ -1,24 +1,32 @@
 <template>
-  <div>
-    <page-header :title="'Packaging'"/>
-    <div class="packaging-home-container">
-      <div class="packaging-home-list">
-        <list-categories/>
-      </div>
-    </div>
+    <div>
+        <page-header :title="'Packaging'" @backClick="goBack" />
+        <div class="packaging-home-container">
+            <div class="packaging-home-list">
+                <list-categories />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import PageHeader from "../../components/common/layout/page-header.vue"
-import ListCategories from '../../components/packaging/list-categories.vue'
+import PageHeader from '../../components/common/layout/page-header.vue';
+import ListCategories from '../../components/packaging/list-categories.vue';
 export default {
-name: 'category-config',
-components:{
- PageHeader,
-ListCategories
-}
-}
+    name: 'category-config',
+    components: {
+        PageHeader,
+        ListCategories
+    },
+    methods: {
+        /**
+         * @description Go back to previous route
+         */
+        goBack() {
+            this.$router.back();
+        }
+    }
+};
 </script>
 <style lang="less">
 @import '../../less/packaging/packaging-home.less';
--- src/pages/packaging/packaging-home.vue
+++ src/pages/packaging/packaging-home.vue
@@ -1,24 +1,32 @@
 <template>
-  <div>
-    <page-header :title="'Packaging'"/>
-    <div class="packaging-home-container">
-      <div class="packaging-home-list">
-        <list-packaging/>
-      </div>
-    </div>
+    <div>
+        <page-header :title="'Packaging'" @backClick="goBack" />
+        <div class="packaging-home-container">
+            <div class="packaging-home-list">
+                <list-packaging />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import PageHeader from "../../components/common/layout/page-header.vue"
-import ListPackaging from '../../components/packaging/list-packaging.vue'
+import PageHeader from '../../components/common/layout/page-header.vue';
+import ListPackaging from '../../components/packaging/list-packaging.vue';
 export default {
-name: 'packaging-home',
-components:{
- PageHeader,
-ListPackaging
-}
-}
+    name: 'packaging-home',
+    components: {
+        PageHeader,
+        ListPackaging
+    },
+    methods: {
+        /**
+         * @description Go back to previous route
+         */
+        goBack() {
+            this.$router.back();
+        }
+    }
+};
 </script>
 <style lang="less">
 @import '../../less/packaging/packaging-home.less';
