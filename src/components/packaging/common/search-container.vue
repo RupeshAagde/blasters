b3999940b7d863d458d08faed614817bc2dc9763
Rohan Shah <rohanshah@gofynd.com>
2022-06-09 15:23:54 +0530

    Reusable search component added

---
+++ src/components/packaging/common/search-container.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="search-box-container">
+        <nitrozen-input
+            class="search-box-input"
+            :id="id"
+            :placeholder="placeholder"
+            :showSearchIcon="true"
+            @input="handleChange"
+        />
+    </div>
+</template>
+
+<script>
+import { NitrozenInput } from '@gofynd/nitrozen-vue';
+export default {
+    name: 'search-container',
+    components: {
+        NitrozenInput
+    },
+    props: {
+        id: {
+            type: Number
+        },
+        placeholder: {
+            type: String
+        },
+        handleChange: {
+            type: Function
+        }
+    }
+};
+</script>
+
+<style lang="less">
+@import '../../../less/packaging/search-container.less';
+</style>
