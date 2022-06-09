7c8470bf28e992ec09a339fb119e7de20f74a2ca
Rupesh <rupesh.aagde@coditas.com>
2022-06-09 18:42:50 +0530

    added general congifuration

--- src/pages/tickets/configuration/generalConfiguration.vue
+++ src/pages/tickets/configuration/generalConfiguration.vue
@@ -15,33 +15,61 @@
                     <nitrozen-input
                         class="number-input"
                         v-model="phoneNumber"
-                        label="Phone Number *"
-                        type="number"
+                        label="Phone Number"
                     ></nitrozen-input>
+                    <!-- <nitrozen-error
+                        @input="onChange($event)"
+                        class="error"
+                        :style="{
+                            visibility:
+                                phoneNumber && !validatePhone
+                                    ? 'visible'
+                                    : 'hidden',
+                        }"
+                        >{{
+                            'Please enter a valid 10 digit phone number'
+                        }}</nitrozen-error
+                    > -->
                     <nitrozen-input
                         class="email-input"
                         v-model="email"
-                        label="Email *"
+                        label="Email"
                     ></nitrozen-input>
+                    <nitrozen-error
+                        class="error"
+                        :style="{
+                            visibility:
+                                email && !validateEmail(email)
+                                    ? 'visible'
+                                    : 'hidden',
+                        }"
+                        >{{ 'Please enter a valid email ID' }}</nitrozen-error
+                    >
                 </div>
             </div>
             <div class="toggle-container">
                 <div class="show-toggle">
                     <label class="label-text"> Show Contact Info </label>
-                    <nitrozen-toggle-btn v-model="show"></nitrozen-toggle-btn>
+                    <nitrozen-toggle-btn
+                        v-model="contactInfoStatus"
+                    ></nitrozen-toggle-btn>
                 </div>
                 <div class="show-toggle">
                     <label class="label-text"> Show Ticket Listing </label>
-                    <nitrozen-toggle-btn v-model="show1"></nitrozen-toggle-btn>
+                    <nitrozen-toggle-btn
+                        v-model="ticketListingStatus"
+                    ></nitrozen-toggle-btn>
                 </div>
                 <div class="show-toggle">
-                    <label class="label-text"> Show Ticket creation </label>
-                    <nitrozen-toggle-btn v-model="show2"></nitrozen-toggle-btn>
+                    <label class="label-text"> Allow Ticket Creation </label>
+                    <nitrozen-toggle-btn
+                        v-model="ticketCreationStatus"
+                    ></nitrozen-toggle-btn>
                 </div>
             </div>
             <div class="btn-container">
                 <nitrozen-button
-                    class="save-tags-btn"
+                    class="save-btn"
                     v-flat-btn
                     :rounded="false"
                     :theme="'secondary'"
@@ -57,13 +85,17 @@
 import {
     NitrozenButton,
     NitrozenInput,
+    NitrozenError,
     NitrozenToggleBtn,
 } from '@gofynd/nitrozen-vue';
 import { PageHeader } from '@/components/common';
+import emailValidator from 'email-validator';
+import SupportService from '../../../services/support.service';
 export default {
     name: 'general-Configuration',
     components: {
         NitrozenButton,
+        'nitrozen-error': NitrozenError,
         PageHeader,
         'nitrozen-toggle-btn': NitrozenToggleBtn,
         'nitrozen-input': NitrozenInput,
@@ -72,14 +104,54 @@ export default {
         return {
             phoneNumber: '',
             email: '',
-            show: '',
-            show1: '',
-            show2: '',
+            contactInfoStatus: false,
+            ticketListingStatus: false,
+            ticketCreationStatus: false,
+            // validatePhone: true,
         };
     },
+    mounted() {
+        this.getGeneralConfiguration();
+    },
     methods: {
+        // onChange(e) {
+        //     var reg = new RegExp('(0|91([- ]?))?[0-9]{10}').test(e);
+        //     console.log(reg, '>>reg');
+        //     this.validatePhone = reg;
+        // },
+        validateEmail(email) {
+            return emailValidator.validate(String(email).toLowerCase().trim());
+        },
         save() {
-            console.log('>>save');
+            let data = {
+                support_phone: this.phoneNumber,
+                support_email: this.email,
+                show_contact_info: this.contactInfoStatus,
+                show_ticket_listing: this.ticketListingStatus,
+                allow_ticket_creation: this.ticketCreationStatus,
+            };
+            SupportService.GeneralConfig(data)
+                .then(() => {
+                    this.$snackbar.global.showSuccess(
+                        'Setting changes successfully'
+                    );
+                })
+                .catch(() => {
+                    this.$snackbar.global.showError('Fails to change setting');
+                });
+        },
+        getGeneralConfiguration() {
+            SupportService.getGeneralConfig()
+                .then((response) => {
+                    this.contactInfoStatus = response.data.show_contact_info;
+                    this.ticketListingStatus =
+                        response.data.show_ticket_listing;
+                    this.ticketCreationStatus =
+                        response.data.allow_ticket_creation;
+                })
+                .catch((err) => {
+                    console.log(err, '>>err');
+                });
         },
     },
 };
@@ -92,23 +164,23 @@ export default {
 .container {
     position: relative;
     background-color: white;
-    margin: 24px;
-    padding: 0px 24px 24px;
+    margin: 5% 22% 35% 22%;
+    padding: 24px;
     top: 56.5px;
 }
 .input {
     position: relative;
-    margin-top: 16px;
+    // margin-top: 16px;
 }
 .number-input {
-    width: 160px;
+    width: 350px;
 }
 .email-input {
-    width: 330px;
-    margin-left: 24px;
+    width: 350px;
 }
 .field {
     display: flex;
+    flex-direction: column;
 }
 .btn-container {
     margin-top: 16px;
@@ -117,13 +189,21 @@ export default {
 }
 .toggle-container {
     display: flex;
-    margin-top: 16px;
+    flex-direction: column;
     justify-content: space-around;
+    margin-top: -24px;
 }
 .label-text {
     padding: 6px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 17px;
+    color: #9b9b9b;
 }
 .show-toggle {
     display: flex;
+    margin-top: 24px;
 }
 </style>
