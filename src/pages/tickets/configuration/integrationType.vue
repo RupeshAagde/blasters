bb1f49a2d4c40c2bc66163d58aaf3d867914dd1f
Rupesh <rupesh.aagde@coditas.com>
2022-06-09 18:42:33 +0530

    added integration type

--- src/pages/tickets/configuration/integrationType.vue
+++ src/pages/tickets/configuration/integrationType.vue
@@ -13,24 +13,38 @@
             <div class="left-section">
                 <div v-if="type.length" class="category">
                     <div
-                        v-for="(integration, index) in type"
+                        v-for="(item, index) in type"
                         v-bind:key="index"
                         class="category-list"
-                        @click="selected(integration)"
+                        @click="selectedItem(index)"
                     >
-                        <!-- <nitrozen-radio
-                            name="metaType"
-                            class="radio-spacing"
-                            :radioValue="'json'"
-
-                        >
-                            
-                        </nitrozen-radio> -->
-                        <div class="card-content-section">
-                            <span class="category-name">{{
-                                integration.name
-                            }}</span>
+                        <div class="card">
+                            <div class="card-logo">
+                                <inline-svg
+                                    v-if="item.logo && item.logo.svg"
+                                    :src="'' + item.logo.svg"
+                                ></inline-svg>
+                            </div>
+                            <div class="card-content">
+                                <span v-if="item.name" class="item-name">{{
+                                    item.name
+                                }}</span>
+                                <span v-if="item.description" class="item-decs">
+                                    {{ item.description }}
+                                </span>
+                            </div>
                         </div>
+                        <div class="selected-action-radio-group">
+                            <nitrozen-radio
+                                name="selectedAction"
+                                v-model="item.value"
+                                :radioValue="item.value"
+                            >
+                            </nitrozen-radio>
+                        </div>
+                        <!-- <div class="card-content-section">
+                            <span class="category-name">{{ item.name }}</span>
+                        </div> -->
                     </div>
                 </div>
             </div>
@@ -80,6 +94,17 @@
                     </div>
                 </div>
             </div>
+            <div class="btn-container">
+                <nitrozen-button
+                    class="save-tags-btn"
+                    v-flat-btn
+                    :rounded="false"
+                    :theme="'secondary'"
+                    @click="selectAndContinue()"
+                >
+                    Select & Continue
+                </nitrozen-button>
+            </div>
         </div>
     </div>
 </template>
@@ -87,19 +112,24 @@
 import {
     NitrozenButton,
     NitrozenInput,
+    NitrozenRadio,
     NitrozenToggleBtn,
 } from '@gofynd/nitrozen-vue';
 import { PageHeader } from '@/components/common';
+import inlinesvg from './../../../components/common/inline-svg.vue';
 export default {
     name: 'integration-type',
     components: {
+        'inline-svg': inlinesvg,
         NitrozenButton,
         PageHeader,
         'nitrozen-toggle-btn': NitrozenToggleBtn,
         'nitrozen-input': NitrozenInput,
+        'nitrozen-radio': NitrozenRadio,
     },
     data() {
         return {
+            // selectedAction: false,
             learningLinksArray: [
                 {
                     title: 'How to install Asset Manager?',
@@ -125,20 +155,45 @@ export default {
             ],
             type: [
                 {
-                    name: 'System categories',
+                    name: 'Kapture Integrations',
+                    logo: { svg: 'kapture' },
+                    description:
+                        'Select from the various choices available for similar products',
+                    value: false,
                 },
                 {
-                    name: 'General configurations',
+                    name: 'Freshdesk Integrations',
+                    logo: { svg: 'headphone' },
+                    description:
+                        'Select from the various choices available for similar products',
+                    value: false,
                 },
-                {
-                    name: 'Choose integration type',
-                },
+                // {
+                //     name: 'None',
+                //     logo: { svg: 'product-icon' },
+                //     description:
+                //         'Select from the various choices available for similar products',
+                // },
             ],
         };
     },
     methods: {
         save() {
             console.log('>>save');
+        },
+        selectedItem(id) {
+
+            let selected = this.type.map((instance, index) => {
+                if (index === id) {
+                    let payload = { ...instance };
+                    payload.value = !payload.value;
+                    return { ...payload };
+                }
+                return instance;
+            });
+
+            this.type = selected;
+
         },
     },
 };
@@ -225,11 +280,12 @@ export default {
     width: 14px;
     margin-left: 5px;
 }
-.left-section {
+.card-logo {
+    display: flex;
 }
 .category-list {
     display: flex;
-    flex-direction: column;
+    justify-content: space-between;
     background-color: white;
     border: 1px solid #e0e0e0;
     border-radius: 4px;
@@ -243,6 +299,40 @@ export default {
 }
 .right-section {
     margin-top: 16px;
+}
+.card-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 8px;
+}
+.item-name {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 140%;
+    color: #41434c;
+}
+.item-decs {
+    margin-top: 4px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 140%;
+    color: #828282;
+}
+.card {
+    display: flex;
+}
+.selected-action-radio-group {
+    display: flex;
+    align-items: center;
+}
+.btn-container {
+    display: flex;
+    justify-content: flex-end;
 }
 </style>
 
