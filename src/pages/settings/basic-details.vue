b92e097b9d82464f83b76d7b0bd0d2a138459cfb
aayush jain <aayushvjain@gofynd.com>
2022-06-15 13:59:23 +0530

    ID: JMKT-594; DONE: 90; HOURS: 8; changes

--- src/pages/settings/basic-details.vue
+++ src/pages/settings/basic-details.vue
@@ -19,7 +19,7 @@
                 v-else-if="pageError && !pageLoading"
                 @tryAgain="fetchSettings"
             ></page-error>
-            <section v-show="!pageLoading && !pageError">
+            <section v-if="!pageLoading && !pageError">
                 <label class="cl-Mako bold-sm title">Basic Details</label>
                 <div class="input-row">
                     <!-- Title -->
@@ -37,7 +37,7 @@
                 <div class="input-row">
                     <!-- Logo -->
                     <div class="input-field">
-                        <div class="n-input-label">Logo *</div>
+                        <!-- <div class="n-input-label">Logo *</div> -->
                         <image-uploader-tile
                             label="Logo"
                             aspectRatio="*"
@@ -53,7 +53,7 @@
                     </div>
                     <!-- Favicon -->
                     <div class="input-field">
-                        <div class="n-input-label">Favicon *</div>
+                        <!-- <div class="n-input-label">Favicon *</div> -->
                         <image-uploader-tile
                             label="Favicon"
                             aspectRatio="*"
@@ -68,6 +68,126 @@
                         </nitrozen-error>
                     </div>
                 </div>
+
+                <div class="input-column">
+                <label class="cl-Mako bold-sm">Authentication Details</label>
+                <div class="input-row">
+                    <!-- Description -->
+                    <div class="input-field">
+                        <nitrozen-input
+                            label="Description *"
+                            type="textarea"
+                            placeholder="Enter Authentication Description"
+                            v-model="authDescription.value"
+                        ></nitrozen-input>
+                        <nitrozen-error v-if="authDescription.showerror">
+                            {{ authDescription.errortext }}
+                        </nitrozen-error>
+                    </div>
+                </div>
+
+                <div class="input-row">
+                      <!-- Authentication Logo -->
+                    <div class="input-field">
+                        <!-- <div class="n-input-label">Authentication Logo *</div> -->
+                        <image-uploader-tile
+                            label="Logo"
+                            aspectRatio="1:1"
+                            @delete="authLogo.value = ''"
+                            @save="authLogo.value = $event"
+                            v-model="authLogo.value"
+                            :fileName="'platform logo'"
+                            namespace="platform-free-logo"
+                        ></image-uploader-tile>
+                        <nitrozen-error v-if="authLogo.showerror">
+                            {{ authLogo.errortext }}
+                        </nitrozen-error>
+                    </div>
+
+                    <!-- Authentication Image -->
+                    <div class="input-field">
+                        <!-- <div class="n-input-label">Authentication Image *</div> -->
+                        <image-uploader-tile
+                            label="Image"
+                            aspectRatio="*"
+                            @delete="authImage.value = ''"
+                            @save="authImage.value = $event"
+                            v-model="authImage.value"
+                            :fileName="'platform logo'"
+                            namespace="platform-feature-image"
+                        ></image-uploader-tile>
+                        <nitrozen-error v-if="authImage.showerror">
+                            {{ authImage.errortext }}
+                        </nitrozen-error>
+                    </div>
+                </div>
+                </div>
+
+
+                <div class="input-column">
+                <label class="cl-Mako bold-sm">Login Details</label>
+                <div class="input-row">
+                    <!-- Description -->
+                    <div class="input-field">
+                        <nitrozen-input
+                            label="Title *"
+                            placeholder="Enter Login Title"
+                            v-model="loginTitle.value"
+                        ></nitrozen-input>
+                        <nitrozen-error v-if="loginTitle.showerror">
+                            {{ loginTitle.errortext }}
+                        </nitrozen-error>
+                    </div>
+                </div>
+
+                <div class="input-row">
+                    <!-- Description -->
+                    <div class="input-field">
+                        <nitrozen-input
+                            label="Description *"
+                            placeholder="Enter Login Description"
+                            v-model="loginDescription.value"
+                        ></nitrozen-input>
+                        <nitrozen-error v-if="loginDescription.showerror">
+                            {{ loginDescription.errortext }}
+                        </nitrozen-error>
+                    </div>
+                </div>
+                </div>
+
+
+                <div class="input-column">
+                <label class="cl-Mako bold-sm">Register Details</label>
+                <div class="input-row">
+                    <!-- Description -->
+                    <div class="input-field">
+                        <nitrozen-input
+                            label="Title *"
+                            placeholder="Enter Register Title"
+                            v-model="registerTitle.value"
+                        ></nitrozen-input>
+                        <nitrozen-error v-if="registerTitle.showerror">
+                            {{ registerTitle.errortext }}
+                        </nitrozen-error>
+                    </div>
+                </div>
+
+                <div class="input-row">
+                    <!-- Description -->
+                    <div class="input-field">
+                        <nitrozen-input
+                            label="Description *"
+                            placeholder="Enter Register Description"
+                            v-model="registerDescription.value"
+                        ></nitrozen-input>
+                        <nitrozen-error v-if="registerDescription.showerror">
+                            {{ registerDescription.errortext }}
+                        </nitrozen-error>
+                    </div>
+                </div>
+                </div>
+
+
                 <!-- <div class="input-column" v-if="basicSettings">
                     <label class="cl-Mako bold-xs title">
                         Whats New Information
@@ -173,6 +293,13 @@ export default {
             title: this.getInitialValue(''),
             logo: this.getInitialValue(''),
             favicon: this.getInitialValue(''),
+            authDescription: this.getInitialValue(''),
+            authImage: this.getInitialValue(''),
+            authLogo: this.getInitialValue(''),
+            loginTitle: this.getInitialValue(''),
+            loginDescription: this.getInitialValue(''),
+            registerTitle: this.getInitialValue(''),
+            registerDescription: this.getInitialValue(''),
             features: [],
             whats_new: []
         };
@@ -200,6 +327,14 @@ export default {
                     this.favicon.value = this.basicSettings.favicon_url;
                     this.whats_new = this.basicSettings.whats_new;
                     this.features = this.basicSettings.features;
+                    this.authDescription.value = this.basicSettings.authentication.panel.description || '';
+                    this.authLogo.value = this.basicSettings.authentication.panel.logo || '';
+                    this.authImage.value = this.basicSettings.authentication.panel.image || '';
+                    
+                    this.loginTitle.value = this.basicSettings.authentication.login.title || 'Login to Fynd Platform';
+                    this.loginDescription.value = this.basicSettings.authentication.login.description || 'Enter account details to manage your online business';
+                    this.registerTitle.value = this.basicSettings.authentication.register.title || 'Welcome to Fynd Platform';
+                    this.registerDescription.value = this.basicSettings.authentication.register.description || 'Enter details to create your online business';
 
                     this.pageError = false;
                     this.pristineData = this.getPayload();
@@ -217,6 +352,20 @@ export default {
             return { showerror: false, value, errortext: '' };
         },
         getPayload() {
+            const authentication = this.basicSettings.authentication
+            authentication.panel = {
+                description: this.authDescription.value,
+                image: this.authImage.value,
+                logo: this.authLogo.value
+            }
+            authentication.login = {
+                title: this.loginTitle.value,
+                description: this.loginDescription.value
+            }
+            authentication.register = {
+                title: this.registerTitle.value,
+                description: this.registerDescription.value
+            }
             return {
                 name: this.title.value,
                 logo_url: this.logo.value,
@@ -227,14 +376,22 @@ export default {
                 ),
                 features: this.basicSettings.features,
                 landingPageDetails:this.basicSettings.landingPageDetails,
-                authentication:this.basicSettings.authentication    
+                authentication
             };
         },
         checkEmpty(key) {
             const emptyErorrs = {
                 title: 'Title is required',
                 logo: 'Logo is required',
-                favicon: 'Favicon is required'
+                favicon: 'Favicon is required',
+                authDescription: 'Description is required',
+                authImage: 'Image is required',
+                authLogo: 'Logo is required',
+                loginTitle: 'Title is required',
+                loginDescription: 'Description is required',
+                registerTitle: 'Title is required',
+                registerDescription: 'Description is required',
+
             };
             if (this[key].value.trim() === '') {
                 this[key].showerror = true;
@@ -325,7 +482,7 @@ export default {
 }
 .input-column {
     .input-row;
-
+    margin: 24px 0 6px 0;
     flex-direction: column;
 }
 .remove-whats-new {
