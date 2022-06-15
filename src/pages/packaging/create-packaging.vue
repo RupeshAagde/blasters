ca037dae8a441628a2d5ecf19fbd5cfcd921a7c4
Rohan Shah <rohanshah@gofynd.com>
2022-06-15 18:17:14 +0530

    Store integrated with packaging home component

--- src/pages/packaging/create-packaging.vue
+++ src/pages/packaging/create-packaging.vue
@@ -16,13 +16,15 @@
                     theme="secondary"
                     title="Save"
                     :disabled="isButtonDisabled"
+                    @click="onSave"
                 >
                     Save
                 </nitrozen-button>
             </div>
         </page-header>
         <div class="create-packaging-container">
-            <packaging-create />
+            <!-- ** Do not remove the ref -->
+            <packaging-create ref="createPackaging" />
         </div>
         <base-modal
             :isOpen="isModalOpen"
@@ -39,6 +41,7 @@
                     class="modal-custom-button"
                     theme="secondary"
                     id="modal-btn"
+                    @click="handleSuccessBtn"
                     >Thank you</nitrozen-button
                 >
             </div>
@@ -52,6 +55,7 @@ import PackagingCreate from '../../components/packaging/packaging-create.vue';
 import { NitrozenButton } from '@gofynd/nitrozen-vue';
 import BaseModal from '../../components/common/dialogs/base-modal.vue';
 import InlineSvg from '../../components/common/inline-svg.vue';
+import { SAVE_PACKAGING_PRODUCT } from '../../store/action.type';
 export default {
     name: 'create-packaging',
     components: {
@@ -64,10 +68,39 @@ export default {
     data() {
         return {
             isButtonDisabled: true,
-            isModalOpen: false
+            isModalOpen: false,
+            showLoader: false
         };
     },
     methods: {
+        /**
+         * @author Rohan Shah
+         * @description Closes the modal button and redirects user to packaging
+         * home page
+         */
+        handleSuccessBtn() {
+            this.isModalOpen = false;
+            this.$router.push('/administrator/packaging/items/');
+        },
+        /**
+         * @author Rohan Shah
+         * @description Calls the child function to get form data
+         * And then uses dispatch method to save the category
+         */
+        onSave() {
+            this.showLoader = true;
+            // call function to get request object
+            const reqObj = this.$refs.createPackaging.savePackagingOrder();
+            this.$store.dispatch(SAVE_PACKAGING_PRODUCT, reqObj).then((res) => {
+                if (res.error) {
+                    return this.$snackbar.global.showError(
+                        'Something went wrong. Failed to add new Packaging product'
+                    );
+                }
+                this.showLoader = false;
+                this.isModalOpen = true;
+            });
+        },
         /**
          * @description Go back to previous route
          */
