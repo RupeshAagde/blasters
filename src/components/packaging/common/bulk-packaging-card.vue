51453a8842954e0af03454690c40e35612dfdc02
Rohan Shah <rohanshah@gofynd.com>
2022-06-30 16:18:11 +0530

    removed labels and changed markup for new design changes

--- src/components/packaging/common/bulk-packaging-card.vue
+++ src/components/packaging/common/bulk-packaging-card.vue
@@ -6,116 +6,122 @@
                 <inline-svg :src="'delete'" class="delete-icon" />
             </div>
         </div>
-        <div class="bulk-card-header">
-            <nitrozen-dropdown
-                :label="'Choose the Categories Config'"
-                :tooltip="
-                    'Select the category whose products can make use of this packaging material in case of a bulk order'
-                "
-                :placeholder="'Select'"
-                :items="groupCategories"
-                @change="(val) => handleDropDownSelect(currentIndex, val)"
-                :value="dropDownValue"
-            />
-            <div class="default-container">
-                Default
-                <nitrozen-toggle-btn
-                    :value="inputs.toggle.val"
-                    @change="
-                        handleToggleChange(currentIndex, inputs.toggle.val)
+        <div class="bulk-card-group-container">
+            <div class="bulk-card-header">
+                <nitrozen-dropdown
+                    :label="'Choose the Categories Config'"
+                    :tooltip="
+                        'Select the category whose products can make use of this packaging material in case of a bulk order'
                     "
-                    :disabled="inputs.toggle.disabled"
+                    :placeholder="'Select'"
+                    :items="groupCategories"
+                    @change="(val) => handleDropDownSelect(currentIndex, val)"
+                    :value="dropDownValue"
                 />
-            </div>
-        </div>
-        <div class="bulk-card-body">
-            <div class="bulk-card-textfield-group">
-                <p>
-                    Volumetric Weight
-                    <nitrozen-tooltip
-                        position="top"
-                        tooltipText="The approx. weight of the bulk package, where max. weight is (Length*Breadth*Height/5000) in kg."
+                <div class="default-container">
+                    Default
+                    <nitrozen-toggle-btn
+                        :value="inputs.toggle.val"
+                        @change="
+                            handleToggleChange(currentIndex, inputs.toggle.val)
+                        "
+                        :disabled="inputs.toggle.disabled"
                     />
-                </p>
-                <div class="row-group">
-                    <div
-                        v-for="(input, index) of Object.keys(
-                            inputs.volumetricWeight
-                        )"
-                        :key="'volumetric-field' + index"
-                        :id="'volumetric-field' + index"
-                        class="row-group-input"
-                    >
-                        <nitrozen-input
-                            class="input w-l"
-                            :label="inputs.volumetricWeight[input].label"
-                            :required="true"
-                            :placeholder="
-                                inputs.volumetricWeight[input].placeholder
-                            "
-                            v-model="inputs.volumetricWeight[input].value"
-                            type="number"
-                            @blur="
-                                handleBlur(
-                                    'volumetricWeight',
-                                    input,
-                                    currentIndex
-                                )
-                            "
-                            @input="
-                                (val) =>
-                                    handleChange(
-                                        'volumetricWeight',
-                                        input,
-                                        val,
-                                        currentIndex
-                                    )
-                            "
-                        />
-                        <nitrozen-error
-                            v-if="inputs.volumetricWeight[input].error"
-                        >
-                            {{ inputs.volumetricWeight[input].error }}
-                        </nitrozen-error>
-                    </div>
                 </div>
             </div>
-            <div class="bulk-card-textfield-group">
-                <p>
-                    Quantity
-                    <nitrozen-tooltip
-                        position="top"
-                        tooltipText="The min. and max. quantity of products belonging to the category, that would fit together in the bulk package"
-                    />
-                </p>
-                <div class="row-group">
-                    <div
-                        v-for="(input, index) of Object.keys(inputs.quantity)"
-                        :key="'quantity-field' + index"
-                        :id="'quantity-field' + index"
-                        class="row-group-input"
-                    >
-                        <nitrozen-input
-                            class="input w-l"
-                            :label="inputs.quantity[input].label"
-                            :required="true"
-                            :placeholder="inputs.quantity[input].placeholder"
-                            v-model="inputs.quantity[input].value"
-                            type="number"
-                            @blur="handleBlur('quantity', input, currentIndex)"
-                            @input="
-                                (val) =>
-                                    handleChange(
-                                        'quantity',
+            <div class="bulk-card-body">
+                <div class="bulk-card-textfield-group">
+                    <p>
+                        Volumetric Weight
+                        <nitrozen-tooltip
+                            position="top"
+                            tooltipText="The approx. weight of the bulk package, where max. weight is (Length*Breadth*Height/5000) in kg."
+                        />
+                    </p>
+                    <div class="row-group">
+                        <div
+                            v-for="(input, index) of Object.keys(
+                                inputs.volumetricWeight
+                            )"
+                            :key="'volumetric-field' + index"
+                            :id="'volumetric-field' + index"
+                            class="row-group-input"
+                        >
+                            <nitrozen-input
+                                class="input w-l"
+                                :required="true"
+                                :placeholder="
+                                    inputs.volumetricWeight[input].placeholder
+                                "
+                                v-model="inputs.volumetricWeight[input].value"
+                                type="number"
+                                @blur="
+                                    handleBlur(
+                                        'volumetricWeight',
                                         input,
-                                        val,
                                         currentIndex
                                     )
-                            "
+                                "
+                                @input="
+                                    (val) =>
+                                        handleChange(
+                                            'volumetricWeight',
+                                            input,
+                                            val,
+                                            currentIndex
+                                        )
+                                "
+                            />
+                            <nitrozen-error
+                                v-if="inputs.volumetricWeight[input].error"
+                            >
+                                {{ inputs.volumetricWeight[input].error }}
+                            </nitrozen-error>
+                        </div>
+                    </div>
+                </div>
+                <div class="bulk-card-textfield-group">
+                    <p>
+                        Quantity
+                        <nitrozen-tooltip
+                            position="top"
+                            tooltipText="The min. and max. quantity of products belonging to the category, that would fit together in the bulk package"
                         />
-                        <nitrozen-error v-if="inputs.quantity[input].error">
-                            {{ inputs.quantity[input].error }}
-                        </nitrozen-error>
+                    </p>
+                    <div class="row-group">
+                        <div
+                            v-for="(input, index) of Object.keys(
+                                inputs.quantity
+                            )"
+                            :key="'quantity-field' + index"
+                            :id="'quantity-field' + index"
+                            class="row-group-input"
+                        >
+                            <nitrozen-input
+                                class="input w-l"
+                                :required="true"
+                                :placeholder="
+                                    inputs.quantity[input].placeholder
+                                "
+                                v-model="inputs.quantity[input].value"
+                                type="number"
+                                @blur="
+                                    handleBlur('quantity', input, currentIndex)
+                                "
+                                @input="
+                                    (val) =>
+                                        handleChange(
+                                            'quantity',
+                                            input,
+                                            val,
+                                            currentIndex
+                                        )
+                                "
+                            />
+                            <nitrozen-error v-if="inputs.quantity[input].error">
+                                {{ inputs.quantity[input].error }}
+                            </nitrozen-error>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -164,10 +170,10 @@ export default {
         handleToggleChange: {
             type: Function
         },
-        groupCategories:{
+        groupCategories: {
             type: Array
         },
-        dropDownValue:{
+        dropDownValue: {
             type: String
         }
     }
