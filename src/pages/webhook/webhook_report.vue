363578f49b79056152739d71b3f379c2c35de155
souravkhurana <souravkhurana@gofynd.com>
2022-08-29 16:07:53 +0530

    ID:FPCO-4070;HOURS:0;DONE:100;prod jiomarket PR for FPCO-4070

--- src/pages/webhook/webhook_report.vue
+++ src/pages/webhook/webhook_report.vue
@@ -2,7 +2,7 @@
     <div class="panel">
         <div class="header-position">
             <adm-page-header @backClick="onCancel" @openHelp="showHelpSection" :title="`Webhook Report`"
-                :contextMenuItems="isOrganisationUser ? [] : contextMenuItems">
+                :contextMenuItems="isOrganisationUser ? [] : contextMenuItems" :noContextMenu="true">
             </adm-page-header>
         </div>
         <loader v-if="startLoader" class="loading"></loader>
