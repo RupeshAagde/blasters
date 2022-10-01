5fd5bb7808b4d9238b0ac7fd22a075f673b5804b
shoab-fynd <mohammedansari@fynd-external.com>
2022-10-01 19:04:17 +0530

    added validations on fields

--- src/pages/product/variants/edit.vue
+++ src/pages/product/variants/edit.vue
@@ -16,7 +16,7 @@
                         <nitrozen-button
                             class="left-space-txb"
                             :theme="'secondary'"
-                            @click="save"
+                            @click="validateForm"
                             v-flatBtn
                             >Save</nitrozen-button
                         >
@@ -82,6 +82,7 @@
                         label="Display Name *"
                         placeholder="Give a name to the variant"
                         v-model="name.value"
+                        @input="validateName"
                     ></nitrozen-input>
                     <nitrozen-error v-if="name.showerror"
                         >{{ name.errortext }}
@@ -107,7 +108,7 @@
             </div>
 
             <!-- image configuration based on display type -->
-            <div v-if="isSwatchSSelected" class="mt-xl">
+            <div v-if="isSwatchSSelected" class="mt-xl suffix">
                 <div class="cust-sh2">Image Configurations</div>
 
                 <div class="input-row mt-l">
@@ -116,7 +117,17 @@
                         <nitrozen-input
                             label="Minimum Height *"
                             placeholder=""
+                            type="number"
                             v-model="image_config.min_height.value"
+                            @input="
+                                checkValidValue(
+                                    'min_height',
+                                    allowedValue.hMin,
+                                    allowedValue.hMax
+                                )
+                            "
+                            :showSuffix="true"
+                            suffix="Pixels"
                         ></nitrozen-input>
                         <nitrozen-error v-if="image_config.min_height.showerror"
                             >{{ image_config.min_height.errortext }}
@@ -128,7 +139,17 @@
                         <nitrozen-input
                             label="Minimum Width *"
                             placeholder=""
+                            type="number"
                             v-model="image_config.min_width.value"
+                            @input="
+                                checkValidValue(
+                                    'min_width',
+                                    allowedValue.wMin,
+                                    allowedValue.wMax
+                                )
+                            "
+                            :showSuffix="true"
+                            suffix="Pixels"
                         ></nitrozen-input>
                         <nitrozen-error v-if="image_config.min_width.showerror"
                             >{{ image_config.min_width.errortext }}
@@ -142,10 +163,20 @@
                         <nitrozen-input
                             label="Maximum Height *"
                             placeholder=""
+                            type="number"
                             v-model="image_config.max_height.value"
+                            @input="
+                                checkValidValue(
+                                    'max_height',
+                                    allowedValue.hMin,
+                                    allowedValue.hMax
+                                )
+                            "
+                            :showSuffix="true"
+                            suffix="Pixels"
                         ></nitrozen-input>
-                        <nitrozen-error v-if="image_config.showerror"
-                            >{{ image_config.errortext }}
+                        <nitrozen-error v-if="image_config.max_height.showerror"
+                            >{{ image_config.max_height.errortext }}
                         </nitrozen-error>
                     </div>
 
@@ -154,7 +185,17 @@
                         <nitrozen-input
                             label="Maximum Width *"
                             placeholder=""
+                            type="number"
                             v-model="image_config.max_width.value"
+                            @input="
+                                checkValidValue(
+                                    'max_width',
+                                    allowedValue.wMin,
+                                    allowedValue.wMax
+                                )
+                            "
+                            :showSuffix="true"
+                            suffix="Pixels"
                         ></nitrozen-input>
                         <nitrozen-error v-if="image_config.max_width.showerror"
                             >{{ image_config.max_width.errortext }}
@@ -168,7 +209,17 @@
                         <nitrozen-input
                             label="Maximum File Size *"
                             placeholder=""
+                            type="number"
                             v-model="image_config.max_size.value"
+                            @input="
+                                checkValidValue(
+                                    'max_size',
+                                    allowedValue.fsMin,
+                                    allowedValue.fsMax
+                                )
+                            "
+                            :showSuffix="true"
+                            suffix="MB"
                         ></nitrozen-input>
                         <nitrozen-error v-if="image_config.max_size.showerror"
                             >{{ image_config.max_size.errortext }}
@@ -177,11 +228,16 @@
 
                     <!-- file type selection -->
                     <div class="row-item">
-                        <nitrozen-input
-                            label="Choose File Type *"
-                            placeholder=""
+                        <nitrozen-dropdown
+                            label="File Type "
+                            placeholder="select file type"
+                            :required="true"
+                            :searchable="true"
+                            :multiple="true"
+                            :items="fileTypeList"
                             v-model="image_config.file_type.value"
-                        ></nitrozen-input>
+                            :enable_select_all="true"
+                        ></nitrozen-dropdown>
                         <nitrozen-error v-if="image_config.file_type.showerror"
                             >{{ image_config.file_type.errortext }}
                         </nitrozen-error>
@@ -195,7 +251,19 @@
         </div>
     </div>
 </template>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.suffix {
+    ::v-deep .nitrozen-input-suffix {
+        background: none;
+        border-left: 0px;
+        font-size: 12px;
+        font-weight: 600;
+        line-height: 16px;
+        letter-spacing: 0em;
+        text-align: left;
+    }
+}
+</style>
 <script>
 import CatalogService from '@/services/catalog.service';
 import loader from '@/components/common/loader';
@@ -226,6 +294,17 @@ const FILE_TYPE = [
     { text: 'JPEG', value: 'jpeg' },
     { text: 'PDF', value: 'pdf' }
 ];
+
+const ALLOWED_VALUE = {
+    hMin: 100,
+    hMax: 1000,
+
+    wMin: 100,
+    wMax: 1000,
+
+    fsMin: 1,
+    fsMax: 20
+};
 
 export default {
     name: 'create-update-variant',
@@ -264,7 +343,8 @@ export default {
             attributeList: [],
             name: this.getInitialValue(''),
             image_config: this.getInitialImageConfig(),
-            fileTypeList: FILE_TYPE
+            fileTypeList: FILE_TYPE,
+            allowedValue: ALLOWED_VALUE
         };
     },
     computed: {
@@ -294,7 +374,7 @@ export default {
             minW = 0,
             maxW = 0,
             maxS = 0,
-            fileT = '',
+            fileT = [],
             mAR = false
         ) {
             return {
@@ -398,6 +478,171 @@ export default {
         }, 500),
 
         save() {},
+        validateForm() {
+            let isValid = true;
+            const imgForm = this.image_config;
+
+            // validate image config if swatch is selected
+            if (this.isSwatchSSelected) {
+                //check if entered maximum height and minimum height values are not contradicting each other if not then validate rest things
+                let heightMinMaxValid = this.minMaxValidation(
+                    'min_height',
+                    'max_height',
+                    this.allowedValue.hMin,
+                    this.allowedValue.hMax
+                )
+                    ? true
+                    : false;
+                isValid = isValid && heightMinMaxValid;
+                if (heightMinMaxValid) {
+                    isValid =
+                        this.checkValidValue(
+                            'min_height',
+                            this.allowedValue.hMin,
+                            this.allowedValue.hMax
+                        ) && isValid;
+                    isValid =
+                        this.checkValidValue(
+                            'max_height',
+                            this.allowedValue.hMin,
+                            this.allowedValue.hMax
+                        ) && isValid;
+                }
+
+                // check if entered maximum width and minimum width values are not contradicting each other if not then validate rest things
+                let widthMinMaxValid = this.minMaxValidation(
+                    'min_width',
+                    'max_width',
+                    this.allowedValue.wMin,
+                    this.allowedValue.wMax
+                )
+                    ? true
+                    : false;
+                isValid = isValid && widthMinMaxValid;
+                if (widthMinMaxValid) {
+                    isValid =
+                        this.checkValidValue(
+                            'min_width',
+                            this.allowedValue.wMin,
+                            this.allowedValue.wMax
+                        ) && isValid;
+                    isValid =
+                        this.checkValidValue(
+                            'max_width',
+                            this.allowedValue.wMin,
+                            this.allowedValue.wMax
+                        ) && isValid;
+                }
+
+                isValid =
+                    this.checkValidValue(
+                        'max_size',
+                        this.allowedValue.fsMin,
+                        this.allowedValue.fsMax
+                    ) && isValid;
+
+                isValid =
+                    this.arrayLength(
+                        this.image_config.file_type,
+                        'file type'
+                    ) && isValid;
+            }
+
+            isValid =
+                this.arrayLength(this.selectedTemplates, 'template') && isValid;
+            isValid =
+                this.arrayLength(this.selectedAttribute, 'attribute') &&
+                isValid;
+            isValid =
+                this.arrayLength(this.selectedDisplayType, 'template') &&
+                isValid;
+            isValid = this.validateName() && isValid;
+
+            return isValid;
+        },
+
+        /*
+        methodName: minMaxValidation
+        paramCount: 4
+            1. First and second params specify property name of input on which we need to perform validation
+            2. Third and fourth params specify the allowed min max range value.
+         */
+        minMaxValidation(key1, key2, range1, range2) {
+            let isValid = true;
+            const enteredMin = parseInt(this.image_config[key1].value);
+            const enteredMax = parseInt(this.image_config[key2].value);
+            const validMin = range1 <= enteredMin && enteredMin <= range2;
+            const validMax = range1 <= enteredMax && enteredMax <= range2;
+            if (validMin && validMax && enteredMin >= enteredMax) {
+                this.image_config[key1].showerror = true;
+                this.image_config[key2].showerror = true;
+                this.image_config[key1].errortext =
+                    'Value should not more than maximum value';
+                this.image_config[key2].errortext =
+                    'Value should not less than minimum value';
+
+                isValid = false;
+            } else {
+                this.image_config[key1].showerror = false;
+                this.image_config[key2].showerror = false;
+                this.image_config[key1].errortext = '';
+                this.image_config[key2].errortext = '';
+            }
+            return isValid;
+        },
+        checkValidValue(key, min = 0, max = 0) {
+            let isValid = true;
+            const val = parseInt(this.image_config[key].value);
+
+            if (val) {
+                if (val < min) {
+                    isValid = false;
+                    this.image_config[key].showerror = true;
+                    this.image_config[
+                        key
+                    ].errortext = `Value should not less than allowed minimum value, which is ${min}`;
+                } else if (val > max) {
+                    isValid = false;
+                    this.image_config[key].showerror = true;
+                    this.image_config[
+                        key
+                    ].errortext = `Value should not more than allowed maximum value, which is ${max}`;
+                } else {
+                    this.image_config[key].showerror = false;
+                }
+            } else {
+                isValid = false;
+                this.image_config[key].showerror = true;
+                this.image_config[key].errortext = 'Field is required';
+            }
+            return isValid;
+        },
+        arrayLength(obj, name) {
+            if (obj.value.length) {
+                obj.showerror = false;
+                return true;
+            } else {
+                obj.showerror = true;
+                obj.errortext = `Choose at least one ${name}`;
+                return false;
+            }
+        },
+        validateName() {
+            const name = this.name.value.trim();
+            if (name) {
+                if (name.length >= 3 && name.length <= 30) {
+                    this.name.showerror = false;
+                    this.name.errortext = '';
+                } else {
+                    this.name.showerror = true;
+                    this.name.errortext =
+                        'Display name should be between 3 to 30 characters';
+                }
+            } else {
+                this.name.showerror = true;
+                this.name.errortext = 'Display name is required';
+            }
+        },
 
         redirectToListing() {
             this.$router.push({ path: '/administrator/product/variants' });
