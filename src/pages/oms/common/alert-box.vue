823b7d248c2a933a3390a5b79941f4f7f2c59130
Rushabh Mulraj Shah <rushabhmshah@gofynd.com>
2023-01-03 20:22:25 +0530

    ID:FPCO-7620; HOURS:2; DONE:70; Adding pages and components [Part 12] - Adding the shipments-list folder

---
+++ src/pages/oms/common/alert-box.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="alert-container" v-if="display">
+        <div class="main-content">
+            <slot name="icon" class="warn-icon">
+                <adm-inline-svg class="warn-icon" src="alerts-blue-icon"></adm-inline-svg>
+            </slot>
+            <slot name="text" class=""></slot>
+        </div>
+        <slot name="action-btn">
+            <div class="close-text" @click="closeAlert">
+                Ok, Got it
+            </div>
+        </slot>
+    </div>
+</template>
+
+<script>
+/* Package imports */
+
+/* Component imports */
+import AdmInlineSvg from '@/components/common/inline-svg.vue';
+import Inlinesvg from '@/components/common/ukt-inline-svg.vue';
+
+export default {
+    name: 'alert-box',
+    props: {
+        display: Boolean
+    },
+    components: {
+        Inlinesvg,
+        AdmInlineSvg
+    },
+    methods: {
+        closeAlert() {
+            this.$emit('close');
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import './../../../less/theme.less';
+
+.alert-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #F5F8FF;
+    border: 1px solid @RoyalBlue;
+    border-radius: 4px;
+    min-height: 46px;
+    padding: 0 8px;
+    margin-bottom: 1rem;
+
+    .main-content {
+        display: flex;
+        align-items: center;
+        font-size: 13px;
+        font-weight: 400;
+
+        .warn-icon {
+            margin-right: 8px;
+        }
+    }
+
+    .close-text {
+        color: @RoyalBlue;
+        font-weight: 700;
+        font-size: 12px;
+        cursor: pointer;
+    }
+}
+</style>
