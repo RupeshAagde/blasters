4afc3d5ded2b8f6e8612304cd7f03a691c251f14
Rushabh Mulraj Shah <rushabhmshah@gofynd.com>
2023-01-03 20:19:19 +0530

    ID:FPCO-7620; HOURS:2; DONE:70; Adding pages and components [Part 11] - Adding the order-details folder

---
+++ src/pages/oms/order-details/order-store-info.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="store-info-container" v-if="store_info && store_info.length">
+        <div
+            class="store-info"
+            v-for="(info, index) in store_info"
+            :key="index"
+        >
+            <span class="sub-heading" v-if="emptyCheck(info.value)"> <span class="title-background">{{ info.display_name }}</span>: </span>{{ info.value }}
+        </div>
+    </div>
+</template>
+
+<script>
+import isEmpty from 'lodash/isEmpty';
+export default {
+    name: 'order-store-info',
+    props: {
+        store_info: {
+         type: Array
+        }
+    },
+     methods:{
+        emptyCheck(val){
+            if(typeof val === 'number' && val !== undefined) return val;
+            else return !isEmpty(val);
+        }
+     }
+}
+</script>
+
+<style lang="less" scoped>
+.store-info-container {
+    .store-info {
+        font-size: 14px;
+        line-height: 22px;
+        word-break: break-word;
+        
+        .sub-heading{
+            .title-background{
+             color: #9B9B9B;
+            }
+        }
+    }
+}
+</style>
