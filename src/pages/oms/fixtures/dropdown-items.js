4be5496a8939eac8f8422dfbf1f339b4bdf84d6e
Rushabh Mulraj Shah <rushabhmshah@gofynd.com>
2023-01-03 16:44:46 +0530

    ID:FPCO-7620; HOURS:2; DONE:70; Adding fixtures and mocks

---
+++ src/pages/oms/fixtures/dropdown-items.js
@@ -0,0 +1,59 @@
+export const fulfillmentCentres = [
+    {
+        text: 'Andheri West',
+        value: 'Andheri West'
+    },
+    {
+        text: 'Sion',
+        value: 'Sion'
+    },
+    {
+        text: 'Ghaziabad',
+        value: 'Ghaziabad'
+    }
+];
+
+export const statuses = [
+    {
+        text: 'All',
+        value: 'all'
+    },
+    {
+        text: 'Completed',
+        value: 'COMPLETED'
+    },
+    {
+        text: 'Partially Completed',
+        value: 'PARTIALLY COMPLETED'
+    },
+    {
+        text: 'Failed',
+        value: 'FAILED'
+    },
+    {
+        text: 'Processing',
+        value: 'PROCESSING'
+    }
+];
+
+export const fileTypes = [
+    {
+        text: 'CSV',
+        value: 'csv'
+    },
+    {
+        text: 'XLSX',
+        value: 'xlsx'
+    }
+];
+
+export const returnNextStates = [
+    {
+        text: 'Initiate RTO',
+        value: 'initiate_rto'
+    },
+    {
+        text: 'Initiate Return',
+        value: 'initiate_return'
+    }
+];
