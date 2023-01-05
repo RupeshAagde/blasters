d3c034f78e620932261bc77443534076e0dcc814
Hitendra Rathore <hitendrarathore@ip-192-168-1-5.ap-south-1.compute.internal>
2023-01-05 17:44:49 +0530

    add sla color for 0

--- src/components/generic-graphs/sla/helper.js
+++ src/components/generic-graphs/sla/helper.js
@@ -2,7 +2,7 @@ import {SLA_CONFIG} from './sla_config.js';
 import moment from 'moment';
 
 export const getSLAColorByPercent = (percent) =>{
-    return SLA_CONFIG.find(slot=>slot.start_time_percentage<percent&&slot.end_time_percentage>=percent)
+    return SLA_CONFIG.find(slot=>slot.start_time_percentage<=percent&&slot.end_time_percentage>=percent)
 } 
 export const displaySlaHoursLeft = (sla_estimated_date) => {    
     const current_time = new Date();
