dc0b58a70411c418fd9d875ff58f8b8fd70414cb
parthmantri <parthmantri@goyfnd.com>
2023-01-09 12:02:49 +0530

    ID:FPCO-8297;HOURS:1;DONE:90; removed commented code

--- src/components/generic-graphs/sla/sla_config.js
+++ src/components/generic-graphs/sla/sla_config.js
@@ -4,16 +4,6 @@ export const SLA_CONFIG = [
         "end_time_percentage": 100,
         "color":"#20ce81" // Mountain Meadow [Greenish]
     },
-    // {
-    //     "start_time_percentage": 50,
-    //     "end_time_percentage": 80,
-    //     "color": "#F2C94C" // Maize (Crayola) [Yellowish]
-    // },
-    // {
-    //     "start_time_percentage": 80,
-    //     "end_time_percentage": 100,
-    //     "color": "#E9783D" // Chinese Orange [Orangish]
-    // },
     {
         "start_time_percentage": 100,
         "end_time_percentage": Infinity,
