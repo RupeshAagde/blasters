a28b287ce8c97c3f13d062a9506548c8c3558ef3
Ophelia Trelawney <opheliatrelawney@gofynd.com>
2023-01-17 13:59:16 +0000

    Fpco 9374 cn dn test cases admin

--- run.local.test.sh
+++ run.local.test.sh
@@ -68,4 +68,5 @@ export NEW_RELIC_LICENSE_KEY=''
 export CDN_ITEM_BASE_PATH=https://cdn.pixelbin.io/v2/falling-surf-7c8bb8/fyndnp/wrkr/
 export OLD_CDN_BASE_URLS=hdn-1.addsale.com
 
-npm run test -- -u
+
+npm run test -- -u 
