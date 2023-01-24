6a15e7bf4297eeded42a50d67b83fbe8da5fcf8d
Rushabh Mulraj Shah <rushabhmshah@gofynd.com>
2023-01-24 15:09:25 +0530

    ID:FPCO-9754; HOURS:1; DONE:100; Resolving bug of item summary displaying blank

--- src/pages/oms/bags-list/more-bag-info.vue
+++ src/pages/oms/bags-list/more-bag-info.vue
@@ -128,7 +128,7 @@
                     v-if="articleData.financial_breakup">
                     <span class="common-key-style">MRP: </span>
                     <span class="common-value-style">
-                        ₹{{ formatPrice(articleData.financial_breakup.reduce((sum, f) => sum + f.price_marked, 0)) }}
+                       ₹{{ formatPrice(articleData.financial_breakup.price_marked * articleData.quantity) }}
                     </span>
                 </div>
 
